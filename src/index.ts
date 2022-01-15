f0340087f62db1dbcdd000fa72fd44bf61775367
nenjo tsu <renenjo.valente@gmail.com>
2022-01-15 23:49:47 +0800

    Update index.ts
    comment CrossDown

--- src/index.ts
+++ src/index.ts
@@ -93,6 +93,6 @@ export  { ichimokucloud, IchimokuCloud }              from './ichimoku/IchimokuC
 export  { keltnerchannels, KeltnerChannels, KeltnerChannelsInput, KeltnerChannelsOutput }              from './volatility/KeltnerChannels';
 export  { chandelierexit, ChandelierExit, ChandelierExitInput, ChandelierExitOutput }              from './volatility/ChandelierExit';
 export  { crossUp, CrossUp }           from './Utils/CrossUp';
-export  { crossDown, CrossDown }       from './Utils/CrossDown';
+// export  { crossDown, CrossDown }       from './Utils/CrossDown';
 
 export  { setConfig, getConfig }       from './config'
