3772fae69be6c92bdcf63bdd2138b2ee794c8241
thao <duongtanthao79@gmail.com>
2022-10-13 18:30:14 +0700

    change data.open.length -> data.close.length

--- src/candlestick/TweezerTop.ts
+++ src/candlestick/TweezerTop.ts
@@ -10,13 +10,13 @@ export default class TweezerTop extends CandlestickFinder {
     }
 
     logic (data:StockData) {
-        return this.upwardTrend(data) && this.approximateEqual(data.high[data.open.length - 2], data.high[data.open.length - 1]);
+        return this.upwardTrend(data) && this.approximateEqual(data.high[data.close.length - 2], data.high[data.close.length - 1]);
     }
 
     upwardTrend (data:StockData) {
         // Analyze trends in closing prices of the first three or four candlesticks
-        let gains = averagegain({ values: data.close.slice(0, data.open.length - 2), period: data.open.length - 3 });
-        let losses = averageloss({ values: data.close.slice(0, data.open.length - 2), period: data.open.length - 3 });
+        let gains = averagegain({ values: data.close.slice(0, data.close.length - 2), period: data.close.length - 3 });
+        let losses = averageloss({ values: data.close.slice(0, data.close.length - 2), period: data.close.length - 3 });
         // Upward trend, so more gains than losses
         return gains > losses;
     }
